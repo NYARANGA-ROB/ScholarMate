45fe5bc9d3a28801d82047cc6caaff9bbfe074fc
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-06-04 09:48:46 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,6 @@ class TestApp(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
         self.client = app.test_client()
         with app.app_context():
-            
             db.create_all()
     
     def tearDown(self):
