eff62530625671d4c1f023e2c1dc40f33955f10e
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-06-04 09:44:46 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,6 +9,7 @@ class TestApp(unittest.TestCase):
         app.config['TESTING'] = True
         
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        
         self.client = app.test_client()
         with app.app_context():
             db.create_all()
