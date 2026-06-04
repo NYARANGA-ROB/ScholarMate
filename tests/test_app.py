6227e4ccd429a7888a97ff3f19e8759700de952f
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-06-04 09:44:11 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,7 @@ import os
 class TestApp(unittest.TestCase):
     
     def setUp(self):
+        
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
         self.client = app.test_client()
