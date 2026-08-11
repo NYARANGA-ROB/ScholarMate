54626192d0b874e2a93bb8ecbdb9c6688e9231d0
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-08-11 17:13:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from datetime import datetime
+
 import os
 from dotenv import load_dotenv
 from groq import Groq
