dda9bc4be91a9770f64e13e9f8f2c39365594bb0
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-08-11 17:15:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_migrate import Migrate
-
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import os
